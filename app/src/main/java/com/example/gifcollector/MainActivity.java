9e608dae358901841b0b369fd5e82eeafbaa7892
merail <rail.mesherov@yandex.ru>
2020-09-11 14:38:38 +0300

    add progressbar while loading gif

--- app/src/main/java/com/example/gifcollector/MainActivity.java
+++ app/src/main/java/com/example/gifcollector/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.gifcollector;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -79,6 +86,19 @@ public class MainActivity extends AppCompatActivity {
 
             Glide.with(getApplicationContext())
                     .load(mGifUrls.get(position))
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            holder.progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            holder.progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+                    })
                     .into(holder.gif);
         }
 
@@ -88,10 +108,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
+            private ProgressBar progressBar;
             private ImageView gif;
 
             ViewHolder(View itemView) {
                 super(itemView);
+                progressBar = itemView.findViewById(R.id.progress);
                 gif = itemView.findViewById(R.id.gif);
             }
         }
